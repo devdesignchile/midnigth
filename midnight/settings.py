1564025a5103dc51ba8ef43577d7256df173fb19
ignCha <rodrigo.chavez@alumnos.ucn.cl>
2025-12-30 22:16:36 -0300

    mail_recover

--- midnight/settings.py
+++ midnight/settings.py
@@ -262,7 +262,7 @@ if DEBUG:
     DEFAULT_FROM_EMAIL = "No Reply <no-reply@midnight.local>"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.titan.email")
     EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
     EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
